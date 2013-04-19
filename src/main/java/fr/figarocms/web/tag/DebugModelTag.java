fc0a7a9cf84d73c4ad7bddc73d82a96795a03ffc
Charles LESCOT <lescot.charles@gmail.com>
2013-04-19 22:43:19 +0200

    [CLE] isolate in a private method ObjectMapper creation and configuration

--- src/main/java/fr/figarocms/web/tag/DebugModelTag.java
+++ src/main/java/fr/figarocms/web/tag/DebugModelTag.java
@@ -1,16 +1,5 @@
 package fr.figarocms.web.tag;
 
-import javax.servlet.http.HttpSession;
-import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.JspWriter;
-import javax.servlet.jsp.PageContext;
-import javax.servlet.jsp.tagext.TagSupport;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.Map;
-
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,6 +11,17 @@ import com.google.common.collect.Maps;
 import org.apache.commons.lang.StringEscapeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.TagSupport;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
 
 public class DebugModelTag extends TagSupport {
     private static final long serialVersionUID = 4611181048692549740L;
@@ -66,100 +66,104 @@ public class DebugModelTag extends TagSupport {
             (DEBUG_JVM_PARAMETER != null) && Boolean.parseBoolean(DEBUG_JVM_PARAMETER);
 
     private static final List<Integer> SCOPES = Lists.newArrayList(
-            Arrays.asList(PageContext.PAGE_SCOPE,PageContext.SESSION_SCOPE, PageContext.REQUEST_SCOPE, PageContext.APPLICATION_SCOPE));
-
+            Arrays.asList(PageContext.PAGE_SCOPE, PageContext.SESSION_SCOPE, PageContext.REQUEST_SCOPE, PageContext.APPLICATION_SCOPE));
 
 
     private Map<String, Object> debugModel = Maps.newHashMap();
 
     private Map<String, Object> debugSession = Maps.newHashMap();
-    private  Map<String, Object> debugRequest = Maps.newHashMap();
+    private Map<String, Object> debugRequest = Maps.newHashMap();
 
-    private  Map<String, Object> debugPage = Maps.newHashMap();
+    private Map<String, Object> debugPage = Maps.newHashMap();
 
-    private  Map<String, Object> debugApplication = Maps.newHashMap();
+    private Map<String, Object> debugApplication = Maps.newHashMap();
 
 
     @Override
     public int doStartTag() throws JspException {
 
         boolean debugOk = DEBUG_FLAG;
-         if (debugOk) {
+        if (debugOk) {
             outputDebugModelInJSON();
-           }
+        }
 
         return SKIP_BODY;
     }
 
     protected void outputDebugModelInJSON() throws JspException {
-            
-            JspWriter out = pageContext.getOut();
 
-            try {
-                out.println(SCRIPT_TYPE_TEXT_JAVASCRIPT_START);
-                for (Integer scope : SCOPES) {
-                    Enumeration attributeNames = pageContext.getAttributeNamesInScope(scope);
+        JspWriter out = pageContext.getOut();
 
-                    while (attributeNames!= null && attributeNames.hasMoreElements()) {
-                        String element = attributeNames.nextElement().toString();
-                        Object attribute = null;
-                        String packagesToExclude = pageContext.getServletContext().getInitParameter(WEBDEBUG_EXCLUDES);
-                        List<String> tokenToFilter = Lists.newArrayList();
-                        if(packagesToExclude!=null){
+        try {
+            out.println(SCRIPT_TYPE_TEXT_JAVASCRIPT_START);
+            for (Integer scope : SCOPES) {
+                Enumeration attributeNames = pageContext.getAttributeNamesInScope(scope);
+
+                while (attributeNames != null && attributeNames.hasMoreElements()) {
+                    String element = attributeNames.nextElement().toString();
+                    Object attribute = null;
+                    String packagesToExclude = pageContext.getServletContext().getInitParameter(WEBDEBUG_EXCLUDES);
+                    List<String> tokenToFilter = Lists.newArrayList();
+                    if (packagesToExclude != null) {
                         tokenToFilter = Arrays.asList(packagesToExclude.split(EXCLUDE_PACKAGE_SEPARATOR));
-                        }
+                    }
 
-                        if (element != null && !ignoredPackage(element, tokenToFilter)) {
+                    if (element != null && !ignoredPackage(element, tokenToFilter)) {
 
-                            if (scope == PageContext.PAGE_SCOPE) {
-                                attribute = pageContext.getAttribute(element);
-                                if (attribute != null) {
-                                    addAttributeToMap(element, attribute, debugPage);
-                                }
-                            }else if (scope == PageContext.REQUEST_SCOPE) {
-                                attribute = pageContext.getRequest().getAttribute(element);
-                                if (attribute != null) {
-                                    addAttributeToMap(element, attribute, debugRequest);
-                                }
-                            } else if (scope == PageContext.SESSION_SCOPE) {
-                                final HttpSession session = pageContext.getSession();
-                                if (session != null) {
+                        if (scope == PageContext.PAGE_SCOPE) {
+                            attribute = pageContext.getAttribute(element);
+                            if (attribute != null) {
+                                addAttributeToMap(element, attribute, debugPage);
+                            }
+                        } else if (scope == PageContext.REQUEST_SCOPE) {
+                            attribute = pageContext.getRequest().getAttribute(element);
+                            if (attribute != null) {
+                                addAttributeToMap(element, attribute, debugRequest);
+                            }
+                        } else if (scope == PageContext.SESSION_SCOPE) {
+                            final HttpSession session = pageContext.getSession();
+                            if (session != null) {
                                 attribute = session.getAttribute(element);
-                                }
-                                if (attribute != null) {
-                                    addAttributeToMap(element, attribute, debugSession);
-                                }
-                            } else if (scope == PageContext.APPLICATION_SCOPE) {
-                                attribute = pageContext.getServletContext().getAttribute(element);
-                                if (attribute != null) {
-                                    addAttributeToMap(element, attribute, debugApplication);
-                                }
+                            }
+                            if (attribute != null) {
+                                addAttributeToMap(element, attribute, debugSession);
+                            }
+                        } else if (scope == PageContext.APPLICATION_SCOPE) {
+                            attribute = pageContext.getServletContext().getAttribute(element);
+                            if (attribute != null) {
+                                addAttributeToMap(element, attribute, debugApplication);
                             }
                         }
                     }
                 }
-
-                debugModel.put(PAGE_REQUEST_KEY, debugPage);
-                debugModel.put(REQUEST_MODEL_KEY, debugRequest);
-                debugModel.put(SESSION_MODEL_KEY, debugSession);
-                debugModel.put(APPLICATION_MODEL_KEY, debugApplication);
-                ObjectMapper objectMapper = new ObjectMapper();
-                objectMapper.setVisibilityChecker(VisibilityChecker.Std.defaultInstance().withFieldVisibility(
-                        JsonAutoDetect.Visibility.ANY));
-                objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-                objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-                objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-                String debugModelAsJSON = null;
-                try {
-                    debugModelAsJSON = objectMapper.writeValueAsString(debugModel);
-                } catch (Throwable t) {
-                    LOGGER.error("error in debugModel serialization in JSON", t);
-                }
-                outputModelInJSON(out, debugModelAsJSON);
-            } catch (IOException e) {
-                throw new JspException("IOException while writing data to page" + e.getMessage(), e);
             }
 
+            debugModel.put(PAGE_REQUEST_KEY, debugPage);
+            debugModel.put(REQUEST_MODEL_KEY, debugRequest);
+            debugModel.put(SESSION_MODEL_KEY, debugSession);
+            debugModel.put(APPLICATION_MODEL_KEY, debugApplication);
+            ObjectMapper objectMapper = getObjectMapper();
+            String debugModelAsJSON = null;
+            try {
+                debugModelAsJSON = objectMapper.writeValueAsString(debugModel);
+            } catch (Throwable t) {
+                LOGGER.error("error in debugModel serialization in JSON", t);
+            }
+            outputModelInJSON(out, debugModelAsJSON);
+        } catch (IOException e) {
+            throw new JspException("IOException while writing data to page" + e.getMessage(), e);
+        }
+
+    }
+
+    private ObjectMapper getObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibilityChecker(VisibilityChecker.Std.defaultInstance().withFieldVisibility(
+                JsonAutoDetect.Visibility.ANY));
+        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        return objectMapper;
     }
 
     private void outputModelInJSON(final JspWriter out, final String debugModelAsJSON) throws IOException {
